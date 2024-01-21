735c8337f72d4ed4ee3c05bd523ffa4056dcedc3
Gaberber <cnhajzwgz@gmail.com>
2024-01-21 15:17:06 +0100

    Update database_manipulation.py

--- database_manipulation.py
+++ database_manipulation.py
@@ -46,7 +46,10 @@ def create_html(df, filename):
 
     csstag = soup.new_tag('link')
     csstag.attrs['rel'] = "stylesheet"
+    # below: one of these css's will not be found...
+    # should modify this to select one css from the right path based on where it's deployed...
     csstag.attrs['href'] = "/static/table_style.css"
+    csstag.attrs['href'] = "./table_style.css"
     soup.head.append(csstag)
 
     titletag = soup.new_tag('title')
